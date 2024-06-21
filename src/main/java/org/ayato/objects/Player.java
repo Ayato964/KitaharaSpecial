72350f92803abe45d8c05611dc13ea9872316a6b
Ayato964 <chtk20063@g.nihon-u.ac.jp>
2024-06-21 14:31:26 +0900

    Ver.0.3.0 Scene - Player.java   移動処理を実装

--- src/main/java/org/ayato/objects/Player.java
+++ src/main/java/org/ayato/objects/Player.java
@@ -1,12 +1,19 @@
 package org.ayato.objects;
 
+import org.ayato.animation.image.ImageMaker;
 import org.ayato.component.ToonObject;
 import org.ayato.component.Transform;
+import org.ayato.util.KeyInputs;
+import org.ayato.util.MouseInputs;
 
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
 
 public class Player extends ToonObject {
     private final String name;
+    private final ImageMaker image = new ImageMaker("objects","player");
     public Player(Transform transform, String name) {
         super(transform);
         this.name = name;
@@ -14,12 +21,16 @@ public class Player extends ToonObject {
 
     @Override
     protected void display(Graphics2D graphics2D, int i, int i1) {
-        graphics2D.setColor(Color.WHITE);
-        graphics2D.fillRect(i, i1, 200, 200);
+        graphics2D.drawImage(image.get(), i, i1, null);
     }
 
     @Override
     protected void tick(Transform transform) {
-
+        move(transform);
     }
+    private void move(Transform transform){
+        if(KeyInputs.get(KeyEvent.VK_LEFT))
+            transform.position.setX(transform.position.getNormalX() - 1);
+    }
+
 }
